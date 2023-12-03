2ede1cbbbebe48f426d03658d855915507585af0
Bishoy Magdy <100763292+bishoyshohdy@users.noreply.github.com>
2023-12-03 12:19:17 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyBUIQSc_gMfasX3J9VS1w_wnIgcQxZ6Am0',
+    authDomain: 'citric-cubist-374512.firebaseapp.com',
+    projectId: 'citric-cubist-374512',
+    storageBucket: 'citric-cubist-374512.appspot.com',
+    messagingSenderId: '866949466540',
+    appId: '1:866949466540:web:4057351714b328e0909209'
+};
+
+// Initialize Firebase
+// eslint-disable-next-line no-unused-vars
+const app = initializeApp(firebaseConfig);
